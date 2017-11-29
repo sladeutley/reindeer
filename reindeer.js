1fd9d1335ed2a7e92ad530bf6c388fdf7b76270f
Slade Utley <SladeUtley@Slades-MacBook-Pro.local>
2017-11-29 15:35:35 -0600

    made it more complicated, trying to be able to pass any array that might be added

--- reindeer.js
+++ reindeer.js
@@ -1,3 +1,5 @@
+console.log("hey");
+
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
@@ -13,9 +15,34 @@ var hohohoElement = document.getElementById("reindeer");
 // Red Dancer
 
 function printToDOM(array) {
+    console.log("hi");
     hohohoElement.innerHTML += array;
 }
 
-for (let i = 0; i < reindeer.length; i++) {
-    printToDOM(colors[i] + " " + reindeer[i] + "<br>" + "<br>");
+function shortestArray(array1, array2) {
+    console.log(array1, array2);
+    let shortestArray = [];
+    if (array1.length < array2.length) {
+        shortestArray = array1.length;
+    } else {
+        shortestArray = array2.length;
+    }
+    return shortestArray
 }
+
+
+let arrayLength = shortestArray(colors, reindeer);
+//I WANT TO MAKE THIS UNIVERSAL, BUT PARAMATERS ARE THE ARRAYS COLORS AND REINDEER NOT ARRAY1 AND ARRAY2
+//wrap for loop in function to make universal
+
+function universalArray(array1, array2) {
+for (let i = 0; i < arrayLength; i++) {
+    console.log(shortestArray);
+    printToDOM(array1[i] + " " + array2[i] + "<br>" + "<br>");
+    }
+}
+universalArray(colors, reindeer);
+
+// for (let i = 0; i < reindeer.length; i++) {
+//     printToDOM(colors[i] + " " + reindeer[i] + "<br>" + "<br>");
+// }
